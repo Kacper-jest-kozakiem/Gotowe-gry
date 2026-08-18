8b48548e736b4944114087c0f6060a81a332ff24
Kapi <kacperrower4@gmail.com>
2026-08-18 13:23:44 +0200

    Opublikowano 1 grę

---
+++ projekt_4/zmienne.py
@@ -0,0 +1,12 @@
+"""
+plik zapisujący stan gry w zmiennych globalnych
+autorem jest Kacper Kliber
+"""
+
+kieszonkowe = 50
+psychika = 80
+zaczerwienienie_janusza = 25
+tekst_fabuly = "Wchodzisz do gabinetu Janusza. Czuć zapach taniej kawy..."
+
+# przedmioty pasywne
+multisport = False
